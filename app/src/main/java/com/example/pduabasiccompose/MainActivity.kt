ad49891175ff93c1065cc4dbd77b405fc21b32b5
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-10-18 13:38:41 +0700

    first commit

---
+++ app/src/main/java/com/example/pduabasiccompose/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.pduabasiccompose
+
+import android.os.Bundle
+import android.widget.Space
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pduabasiccompose.ui.theme.PDuaBasicComposeTheme
+import org.intellij.lang.annotations.JdkConstants.FontStyle
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PDuaBasicComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicColoumn(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicColoumn(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 50.dp)) {
+        Text("Hello",
+            fontSize = 100.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.padding(18.dp))
+        Text("JETPACK COMPOSE",
+            fontSize = 40.sp,
+            fontStyle = androidx.compose.ui.text.font.FontStyle.Italic,
+            )
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.coba),
+            contentDescription = "",
+            modifier = Modifier.clip(CircleShape))
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PDuaBasicComposeTheme {
+        BasicColoumn()
+    }
+}
